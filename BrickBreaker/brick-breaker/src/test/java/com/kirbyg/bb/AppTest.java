7667b0b2db5c159238ab58977fb1f76df1651458
Kirby Gordon <kirbyjgordon@gmail.com>
2026-03-16 20:53:06 -0400

    add untracked files

---
+++ BrickBreaker/brick-breaker/src/test/java/com/kirbyg/bb/AppTest.java
@@ -0,0 +1,20 @@
+package com.kirbyg.bb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
